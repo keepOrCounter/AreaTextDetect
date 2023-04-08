94dec938a40e2bb957caef6dd356c290c4f1e4e7
keepOrCounter <leolhc@qq.com>
2023-04-08 17:40:20 +0100

    Devide event class into two types
    Add tkwindows class

--- sub.py
+++ sub.py
@@ -1,22 +1,63 @@
-import pynput,time
+import pynput,time,tkinter
 # import threading
 
 
-class eventLis():
+class eventKeyboard():
+    def __init__(self) -> None:
+        self.keyValue=-1
+        self.timeIntervalStart=0
+        self.timeIntervalEnd=0
+        self.activeFlag=-1
+        self.keyPressed=pynput.keyboard.Listener(on_press=self.pressed)
+        
+    def pressed(self,key):
+        # print("b")
+        
+        if str(type(key))=="<enum 'Key'>":
+            if key.name=="alt_l":
+                self.keyValue=key.name
+                print(key.name)
+        else:
+            self.keyValue=key.char
+            print(key.char=="z")
+        
+        self.timeIntervalEnd=time.time()
+        
+        if self.timeIntervalEnd-self.timeIntervalStart>10.0:
+            print("Times Up")
+            if self.activeFlag==-1:
+                print("Exit")
+                return False
+            self.timeIntervalStart=time.time()
+            self.activeFlag=-1
+        
+    def StartListener(self) -> None:
+        self.keyPressed.start()
+        self.timeIntervalStart=time.time()
+            
+    def terminate(self) -> None:
+        self.keyPressed.stop()
+        
+    def keyGet(self) -> str:
+        return self.keyValue
+    
+    def activeFlagSet(self,newFlag) -> None:
+        self.activeFlag=newFlag
+
+
+class eventMouse():
     def __init__(self) -> None:
         self.timeIntervalStart=0
         self.timeIntervalEnd=0
-        self.detectFlag=-1
+        self.activeFlag=-1
         self.DetectedMouseXPos=-1
         self.DetectedMouseYPos=-1
-        self.keyValue=-1
-        
-        self.keyPressed=pynput.keyboard.Listener(on_press=self.pressed)
         
         # keyPressed.stop()
         self.mouseClicked=pynput.mouse.Listener(on_click=self.clicked)
         
         # mouseClicked.join()
+
         
     def clicked(self, x, y, button, pressed):
         
@@ -24,40 +65,87 @@ class eventLis():
             self.DetectedMouseXPos=x
             self.DetectedMouseYPos=y
             print(self.DetectedMouseXPos,self.DetectedMouseYPos)
+            
+            
+        self.timeIntervalEnd=time.time()
+        print(self.timeIntervalEnd-self.timeIntervalStart)
+        if self.timeIntervalEnd-self.timeIntervalStart>10.0:
+            print("Times Up")
+            if self.activeFlag==-1:
+                print("exit")
+                return False
+            self.timeIntervalStart=time.time()
+            self.activeFlag=-1
             # if x==0 or y ==0:
             #     self.detectFlag=False
                 # self.terminate()
             # return True
 
-    def pressed(self,key):
-        self.keyValue=key
-        if str(type(key))=="<enum 'Key'>":
-            if key.name=="alt_l":
-                print(key.name)
-        else:
-            print(key.char=="z")
+
         # return True
 
-    def clickListener(self) -> None:
-        self.keyPressed.start()
+    def StartListener(self) -> None:
+        self.timeIntervalStart=time.time()
         self.mouseClicked.start()
         
     def terminate(self) -> None:
-        self.keyPressed.stop()
         self.mouseClicked.stop()
         
     def mouseGet(self) -> int:
         return self.DetectedMouseXPos,self.DetectedMouseYPos
+    
+    def activeFlagSet(self,newFlag) -> None:
+        self.activeFlag=newFlag
+        
+class windowsUI():
+    def __init__(self,override=False,alpha=0.1,bgColor="black",\
+        width=-1,height=-1,positionX=0,positionY=0,screenShot=True) -> None:
+        self.__timeList=[time.time(),0]
+        self.__counter=0
+        
+        self.__root = tkinter.Tk()
+        self.__root.overrideredirect(override)
+        self.__root.attributes("-alpha", alpha)
+        if width==-1:
+            width=self.__root.winfo_screenwidth()
+        if height==-1:
+            height=self.__root.winfo_screenheight()
+            
+        self.__root.geometry("{0}x{1}+{2}+{3}"\
+            .format(width, height,positionX,positionY))
+        self.__root.configure(bg=bgColor)
+        self.keeper()
+        self.__root.mainloop()
+        
+    def keeper(self) -> None:
+        self.__counter+=1
+        self.__timeList[self.__counter%2]=time.time()
+        print(self.__timeList[self.__counter%2]-self.__timeList[(self.__counter-1)%2])
+        
+        print("101010")
+        if self.__counter>10:
+            self.closeWindow()
+        else:
+            self.__root.after(1000, self.keeper)
+        
+    def closeWindow(self) -> None:
+        self.__root.destroy()
 
-    def keyGet(self):
-        return self.keyValue
 
 if __name__=="__main__":
-    startEvent=eventLis()
-    startEvent.clickListener()
-    x,y=startEvent.mouseGet()
-    while x!=0 and y!=0:
-        time.sleep(0.1)
-        x,y=startEvent.mouseGet()
-    startEvent.terminate()
+    # startEvent=eventKeyboard()
+    # startEvent.StartListener()
+    # # x,y=startEvent.mouseGet()
+    # # while x!=0 and y!=0:
+    # #     time.sleep(0.1)
+    # #     x,y=startEvent.mouseGet()
+    # time.sleep(9)
+    # startEvent.activeFlagSet(1)
+    # time.sleep(9)
+    # # startEvent.activeFlagSet(1)
+    # # time.sleep(15)
+    # startEvent.terminate()
+    wind=windowsUI(True,0.1,"black")
+    # time.sleep(5)
+    
     print("a")
