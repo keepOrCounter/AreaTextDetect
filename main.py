f4f5e6f9584079f3bd1ee1ff763278ce0d8fb21e
keepOrCounter <leolhc@qq.com>
2023-06-18 23:04:35 +0800

    add code note
    update keyboard listener

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sub
 
 
-mouseL=sub.eventMouse()
-wind=sub.windowsUI(True,0.5,"black",listener=mouseL,screenShot=1)
+# mouseL=sub.eventMouse()
+# wind=sub.windowsUI(True,0.5,"black",listener=mouseL,screenShot=1)
+wind=sub.windowsUI(True,0.5,"white",screenShot=2,width=1000,height=500,positionX=50,positionY=50)
